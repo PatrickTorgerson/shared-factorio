7a1a46fa601f98d02f0c13f83f75321c9dab6fe1
TheOneThatIs <theonethatis720@gmail.com>
2025-08-09 01:22:19 -0700

    Fixed install.bat
    Did that.

--- install.bat
+++ install.bat
@@ -1,3 +1,2 @@
 @echo off
-xcopy ".\[virtual-signal=signal-rightwards-leftwards-arrow] - swap world.zip"\
-    "C:\Users\jonat\AppData\Roaming\Factorio\saves\" /K /D /H /Y
+xcopy ".\[virtual-signal=signal-rightwards-leftwards-arrow] - swap world.zip" "C:\Users\jonat\AppData\Roaming\Factorio\saves\" /K /D /H /Y
