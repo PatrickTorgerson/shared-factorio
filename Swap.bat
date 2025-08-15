622441cbf52cd6f7c1516409c2e0144b5f3e6907
TheOneThatIs <theonethatis720@gmail.com>
2025-08-15 04:15:17 -0700

    TheOneThatIs: Session 2

---
+++ Swap.bat
@@ -0,0 +1,2 @@
+@echo off
+xcopy "C:\Users\jonat\AppData\Roaming\Factorio\saves\[virtual-signal=signal-rightwards-leftwards-arrow] - swap world.zip" "C:\Users\jonat\OneDrive\Desktop\FactorioShared" /K /D /H /Y
